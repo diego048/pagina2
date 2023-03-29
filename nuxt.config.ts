6c1cd6d4a10428329fdfd82082ced74ac202444c
diego048 <103468763+diego048@users.noreply.github.com>
2023-03-29 01:24:04 -0500

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    routeRules: {'/admin/**': { ssr: false },},
     css:['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
     build: {
         transpile: ['vuetify']
