1a955208bddc40abd2ea3bccafe3f5b3ac2003b1
diego048 <103468763+diego048@users.noreply.github.com>
2023-03-30 08:09:54 -0500

    Update navigation.vue

--- components/navigation.vue
+++ components/navigation.vue
@@ -40,7 +40,7 @@
                 <NuxtLink class="link" to="/">
                   <v-img
                     class="mx-2"
-                    :src="p1"
+                    :src="/imag/imagen37.png"
                     style="width: 90%"
                     contain
                   ></v-img>
